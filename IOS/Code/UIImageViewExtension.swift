a9c06f8e9b2643629c3b7db30e116342d7ae221b
武云辉 <1104859447@qq.com>
2020-08-10 15:11:48 +0800

    FIX: uiimage view extension

--- IOS/Code/UIImageViewExtension.swift
+++ IOS/Code/UIImageViewExtension.swift
@@ -1,12 +1,15 @@
 import UIKit
 
 extension UIImageView {
+    /// 异步加载图片的最大并发数
+    private static let load_semaphore: DispatchSemaphore = DispatchSemaphore(value: 5)
+
     /// 从 url 异步加载图片
     /// - Parameters:
     ///   - url: 图片 url
     ///   - placeholder: 默认图片
     ///   - queue: 图片加载的队列
-    func load(url: String, placeholder: UIImage? = nil, queue: DispatchQueue? = nil) {
+    func load(url: String, placeholder: UIImage? = nil, onFailed: ((_ error: Error) -> ())? = nil) {
         image = placeholder
 
         guard let urlStr = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: urlStr) else {
@@ -16,34 +19,53 @@ extension UIImageView {
         var imgData: Data?
 
         let download = DispatchWorkItem {
-            let semaphore = DispatchSemaphore(value: 0)
+            Self.load_semaphore.wait()
             defer {
-                semaphore.wait()
+                Self.load_semaphore.signal()
             }
 
-            URLSession.shared.dataTask(with: url) { data, _, error in
-                defer {
-                    semaphore.signal()
+            let result = URLSession.shared.synchronousDataTask(with: url)
+            if let error = result.2 {
+                if let onFailed = onFailed {
+                    onFailed(error)
                 }
-                if let error = error {
-                    print(error.localizedDescription, "image url: \(url.absoluteString)")
-                    return
-                }
-                imgData = data
-            }.resume()
+                return
+            }
+            imgData = result.0
         }
 
-        let load = DispatchWorkItem(qos: .userInteractive) {[weak self] in
+        let load = DispatchWorkItem(qos: .userInteractive) { [weak self] in
             guard let data = imgData else { return }
             let img = UIImage(data: data)
             self?.image = img
         }
         download.notify(queue: .main, execute: load)
 
-        var imgQueue: DispatchQueue?
-        if queue == nil {
-            imgQueue = DispatchQueue(label: "img_download", qos: .utility)
+        let queue = DispatchQueue(label: "image_load_queue", qos: .utility, attributes: [.concurrent])
+        queue.async(execute: download)
+    }
+}
+
+private extension URLSession {
+    /// 同步请求
+    func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let dataTask = self.dataTask(with: url) {
+            data = $0
+            response = $1
+            error = $2
+
+            semaphore.signal()
         }
-        imgQueue?.async(execute: download)
+        dataTask.resume()
+
+        _ = semaphore.wait(timeout: .distantFuture)
+
+        return (data, response, error)
     }
 }
